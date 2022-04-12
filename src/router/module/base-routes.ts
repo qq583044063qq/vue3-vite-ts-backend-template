5e18b7c9a9cecb7cd45960b6648fe287d6379390
Hibiki -khorosho <583044063@qq.com>
2022-04-12 15:14:54 +0800

    修复一些路由配置错误

--- src/router/module/base-routes.ts
+++ src/router/module/base-routes.ts
@@ -7,17 +7,17 @@ export default [
     meta: { title: '工作空间' },
     children: [
       {
-        path: '/analysis',
+        path: 'analysis',
         component: () => import('../../views/Analysis/index.vue'),
         meta: { title: '分析页' },
       },
       {
-        path: '/console',
+        path: 'console',
         component: () => import('../../views/Console/index.vue'),
         meta: { title: '控制台' },
       },
       {
-        path: '/dashbord',
+        path: 'dashbord',
         component: () => import('../../views/Dashbord/index.vue'),
         meta: { title: '仪表盘'},
       }
@@ -29,22 +29,22 @@ export default [
     meta: { title: '错误'},
     children:[
       {
-        path: '/401',
+        path: '401',
         component: () => import('../../views/Error/401.vue'),
         meta: { title: '401' }
       },
       {
-        path: '/403',
+        path: '403',
         component: () => import('../../views/Error/403.vue'),
         meta: { title: '403' },
       },
       {
-        path: '/404',
+        path: '404',
         component: () => import('../../views/Error/404.vue'),
         meta: { title: '404' },
       },
       {
-        path: '/500',
+        path: '500',
         component: () => import('../../views/Error/500.vue'),
         meta: { title: '500' },
       }
@@ -52,22 +52,22 @@ export default [
   },
   {
     path: '/system',
-    redirect: '/system/index',
+    redirect: 'index',
     component: BaseLayout,
     meta: { title: '系统管理' },
     children: [
       {
-        path: '/system/index',
+        path: 'index',
         component: () => import('../../views/System/index.vue'),
         meta: { title: '系统管理主页' },
       },
       {
-        path: '/system/user',
+        path: 'user',
         component: () => import('../../views/System/user/index.vue'),
         meta: { title: '用户管理' },
       },
       {
-        path: '/system/role',
+        path: 'role',
         component: () => import('../../views/System/role/index.vue'),
         meta: { title: '角色管理' },
       }
@@ -78,12 +78,12 @@ export default [
     meta: { title: '错误页面' },
     children: [
       {
-        path: '/result/success',
+        path: 'success',
         component: () => import('../../views/Result/success.vue'),
         meta: { title: '成功页面' },
       },
       {
-        path: '/result/failure',
+        path: 'failure',
         component: () => import('../../views/Result/failure.vue'),
         meta: { title: '失败页面' },
       },
