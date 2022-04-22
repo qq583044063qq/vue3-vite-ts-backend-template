ef750669009e4dc105de723e74e3f83c50b53136
Hibiki -khorosho <583044063@qq.com>
2022-04-22 16:28:02 +0800

    添加元素遮罩loading组件

---
+++ src/components/LoadingOverMark/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import loadingImage from '@/assets/images_load.gif'
+</script>
+<template>
+  <div class="overMark-container">
+    <div class="loading">
+      <span class="icon"><img :src="loadingImage"/></span>
+      <span class="text">正在加载中...</span>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.overMark-container{
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  .loading {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    .icon {
+      width: 200px;
+      height: 200px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .text {
+      color: #999;
+      font-size: 16px;
+    }
+  }
+}
+</style>
